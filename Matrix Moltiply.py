82325221fc2d3087b3c7dcaa1a6b847a37c24d75
Mahdi Ghomsheh <kitkat.helix@gmail.com>
2021-07-05 16:06:42 +0430

    the new update

--- Matrix Moltiply.py
+++ Matrix Moltiply.py
@@ -13,11 +13,49 @@ def multiply_matrix(matrix_a, matrix_b) -> List[List[float]]:
     return result
 
 
-matrix_1 = [[2, 3, 6],
-            [5, -1, 12]]
+row_matrix_1 = int(input("Please insert the number of rows in matrix one: "))
+column_matrix_1 = int(input("Please insert the number of column in matrix one: "))
 
-matrix_2 = [[1, 0, 4, 7],
-            [2, -3, 6, 2],
-            [5, 8, 9, 6]]
+row_matrix_2 = int(input("please insert the number of rows in matrix two"))
+column_matrix_2 = int(input("please insert the number of column in matrix two"))
 
-print(multiply_matrix(matrix_1, matrix_2))
+matrix_1 = []
+matrix_2 = []
+
+print("\nPlease insert the value in the matrix 1: ")
+
+for row in range(row_matrix_1):
+    temp = []
+    for column in range(column_matrix_1):
+        temp.append(int(input()))
+    matrix_1.append(temp)
+
+for row in range(row_matrix_1):
+    for column in range(column_matrix_1):
+        print(matrix_1[row][column], end=" ")
+    print()
+
+
+
+print("\nPlease insert the value in the matrix 2: ")
+
+for row in range(row_matrix_2):
+    temp = []
+    for column in range(column_matrix_2):
+        temp.append(int(input()))
+    matrix_2.append(temp)
+
+
+for row in range(row_matrix_2):
+    for column in range(column_matrix_2):
+        print(matrix_1[row][column], end=" ")
+    print()
+
+
+if column_matrix_1 == row_matrix_2:
+    print(f"\nmultiply_matrix(matrix_1, matrix_2)")
+else:
+    print("multiplication in not possible, row_matrix_2 must be equal with column_matrix_1")
+
+
+
