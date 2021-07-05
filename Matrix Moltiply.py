dc3b8180dc11b7ea0b760cd8c4cccda8ea086304
Mahdi Ghomsheh <kitkat.helix@gmail.com>
2021-07-05 14:15:50 +0430

    updated matrix multiply

--- Matrix Moltiply.py
+++ Matrix Moltiply.py
@@ -1,19 +1,23 @@
-matrix_a = [[2, 3, 6],
+from typing import List
+
+
+def multiply_matrix(matrix_a, matrix_b) -> List[List[float]]:
+
+    result = [[0 for j in range(len(matrix_b[0]))] for i in range(len(matrix_a))]
+
+    for row in range(len(matrix_a)):
+        for column in range(len(matrix_b[0])):
+            for k in range(len(matrix_b)):
+                result[row][column] += matrix_a[row][k] * matrix_b[k][column]
+
+    return result
+
+
+matrix_1 = [[2, 3, 6],
             [5, -1, 12]]
 
-matrix_b = [[1, 0, 4, 7],
+matrix_2 = [[1, 0, 4, 7],
             [2, -3, 6, 2],
             [5, 8, 9, 6]]
 
-result = [[0, 0, 0, 0],
-          [0, 0, 0, 0]]
-
-
-for row in range(len(matrix_a)):
-    for column in range(len(matrix_b[0])):
-        for k in range(len(matrix_b)):
-            result[row][column] += matrix_a[row][k] * matrix_b[k][column]
-
-
-for member in result:
-    print(member)
+print(multiply_matrix(matrix_1, matrix_2))
